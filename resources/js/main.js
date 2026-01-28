fd6d8a672020b867a5310fb47f1fd5d5de9bbf5d
IamBritex <del.alc.jav@gmail.com>
2026-01-28 15:39:03 -0600

    aaaaa

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,9 +1,26 @@
 import { auth } from "./config/firebase-config.js";
 import { onAuthStateChanged } from "firebase/auth";
 import { showLoader, hideLoader } from "./modules/loader-ui.js";
-import { initEmojiSystem } from "./utils/emoji-handler.js"; // Importar nuevo módulo
+import { initEmojiSystem } from "./utils/emoji-handler.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+    // --- LIMPIEZA DE CACHÉ Y SERVICE WORKERS ---
+    // Elimina Service Workers y cachés previos para evitar errores de versión
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.getRegistrations().then(registrations => {
+            for (let registration of registrations) {
+                registration.unregister();
+            }
+        });
+    }
+
+    if ('caches' in window) {
+        caches.keys().then(keys => {
+            keys.forEach(key => caches.delete(key));
+        });
+    }
+    // ------------------------------------------------
+
     showLoader();
     
     // Iniciar sistema de emojis globalmente
